ac9fc8235d4bfc6c5dac7d41ed0274b6b7441139
Satish Kumar Sharma <skwebserv@gmail.com>
2025-04-06 07:47:41 +0530

    Removed session from validation because it take in controller

--- app/Http/Requests/StoreResultRequest.php
+++ app/Http/Requests/StoreResultRequest.php
@@ -24,22 +24,22 @@ class StoreResultRequest extends FormRequest
     public function rules()
     {
         return [
-            'admit_card_id'=>'required',
-            'session'=>'required',
-            'roll'=>'required',
+            'admit_card_id' => 'required',
+            // 'session'=>'required',
+            'roll' => 'required',
             'class' => 'required|in:Play,Nursery,LKG,UKG,Std.1,Std.2,Std.3,Std.4,Std.5',
-			'hindi' => 'required|numeric',
-			'english' => 'required|numeric',
-			'maths' => 'required|numeric',
-			'drawing' => 'required|numeric',
-			'total' => 'required|numeric',
-			'full_marks' => 'required|numeric',
-			'computer' => 'required_if:class,Std.1,Std.2,Std.3,Std.4,Std.5|numeric',
-			'science' => 'required_if:class,Std.1,Std.2,Std.3,Std.4,Std.5|numeric',
-			'science_oral' => 'required_if:class,Std.1,Std.2,Std.3,Std.4,Std.5|numeric',
-			'sst' => 'required_if:class,Std.1,Std.2,Std.3,Std.4,Std.5|numeric',
-			'sst_oral' => 'required_if:class,Std.1,Std.2,Std.3,Std.4,Std.5|numeric',
-			'gk' => 'required_if:class,Std.1,Std.2,Std.3,Std.4,Std.5|numeric',
+            'hindi' => 'required|numeric',
+            'english' => 'required|numeric',
+            'maths' => 'required|numeric',
+            'drawing' => 'required|numeric',
+            'total' => 'required|numeric',
+            'full_marks' => 'required|numeric',
+            'computer' => 'required_if:class,Std.1,Std.2,Std.3,Std.4,Std.5|numeric',
+            'science' => 'required_if:class,Std.1,Std.2,Std.3,Std.4,Std.5|numeric',
+            'science_oral' => 'required_if:class,Std.1,Std.2,Std.3,Std.4,Std.5|numeric',
+            'sst' => 'required_if:class,Std.1,Std.2,Std.3,Std.4,Std.5|numeric',
+            'sst_oral' => 'required_if:class,Std.1,Std.2,Std.3,Std.4,Std.5|numeric',
+            'gk' => 'required_if:class,Std.1,Std.2,Std.3,Std.4,Std.5|numeric',
         ];
     }
 }
